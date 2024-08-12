34d944d0841577f080ef685668ff649483ed3c62
Rodolfo Tenfen <rodolfo.tenfen@gmail.com>
2024-08-12 00:49:29 -0300

    Reformat code following Ktlint's requirements

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
 
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
+                "proguard-rules.pro",
             )
         }
     }
